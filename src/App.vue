3b70e06c5d53e98c2978d1089b935d7e1a8bcd15
Kholodov.d <kholodov.d@gmail.com>
2020-05-05 19:22:45 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>{{ title }}</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group w-100">
+          <label for="name">Имя</label>
+          <input type="text" id="name" class="form-control" v-model="name" />
+        </div>
+        <div class="form-group w-100">
+          <label for="surname">Фамилия</label>
+          <input
+            type="text"
+            id="surname"
+            class="form-control"
+            v-model="surname"
+          />
+        </div>
+        <div class="form-group w-100">
+          <label for="phone">Телефон</label>
+          <input type="text" id="phone" class="form-control" v-model="phone" />
+        </div>
+        <div class="btn btn-success" @click="onSave()">Сохранить</div>
+      </div>
+      <div class="row mt-1" v-for="note in notes" :key="note.id">
+        <div class="col-4">{{ note.name }}</div>
+        <div class="col-4">{{ note.surname }}</div>
+        <div class="col-4">{{ note.phone }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      title: 'myApp',
+      name: '',
+      surname: '',
+      phone: '',
+      notes: [],
+    };
+  },
+  components: {},
+  methods: {
+    async onSave() {
+      let note = {
+        name: this.name,
+        surname: this.surname,
+        phone: this.phone,
+        notes: [],
+      };
+      try {
+        await this.$http.post('http://localhost:3000/notes', note);
+        this.updateData();
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async updateData() {
+      try {
+        await this.$http
+          .get('http://localhost:3000/notes')
+          .then((res) => res.json())
+          .then((res) => (this.notes = res));
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+  created() {
+    this.updateData();
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
